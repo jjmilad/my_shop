717b0afe975ac08c9d2cb1d37408c2ceee12c2a2
milad <jj@milads-mbp.speedport.ip>
2023-07-25 22:43:12 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="w-full min-h-screen">
       <div className="index-bg h-[320px]">
         <div className='flex justify-center items-center container mx-auto md:w-[70%] relative px-6'>
-            <div className="absolute right-6 md:right-0 bg-primary md:py-[7px] py-[6px] rounded-r-full md:rounded-r-xl px-4 top-[50%] translate-y-[-36%]">
+            <div className="absolute right-6 md:right-0 bg-primary md:py-[8px] py-[9px] rounded-r-full md:rounded-r-xl px-4 top-[50%] translate-y-[-37%]">
               <IoSearch className="w-7 h-7 md:w-8 md:h-8 fill-white"/>
             </div>
             <input className='rounded-full md:rounded-xl mt-5 mb-2 border py-[10px] md:py-[11px] px-5  w-full outline-none shadow-lg  ' placeholder='Search any term you like'/>
