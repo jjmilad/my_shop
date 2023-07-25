bbde86fb674e4d9d9b1398d75d2bc80512bb224e
milad <jj@milads-mbp.speedport.ip>
2023-07-25 22:40:35 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
             <div className="absolute right-6 md:right-0 bg-primary md:py-[7px] py-[6px] rounded-r-full md:rounded-r-xl px-4 top-[50%] translate-y-[-36%]">
               <IoSearch className="w-7 h-7 md:w-8 md:h-8 fill-white"/>
             </div>
-            <input className='rounded-full md:rounded-xl mt-5 mb-2 border py-[8px] md:py-[11px] px-5  w-full outline-none shadow-lg text-[15px]' placeholder='Search any term you like'/>
+            <input className='rounded-full md:rounded-xl mt-5 mb-2 border py-[8px] md:py-[11px] px-5  w-full outline-none shadow-lg  ' placeholder='Search any term you like'/>
         </div>
         <IndexBrandsDiv/>
         <IndexPostsDiv/>
