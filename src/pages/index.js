c8671c5711c003f351f211d63a85d1198e02a75c
milad <jj@milads-mbp.speedport.ip>
2023-08-07 09:56:14 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,11 @@ export default function Home() {
   return (
     <div className="w-full min-h-screen">
       <div className="index-bg h-[320px]">
-        <div className='flex justify-center items-center container mx-auto md:w-[70%] relative px-6'>
-            <div className="absolute right-6 md:right-0 bg-primary md:py-[8px] py-[9px] rounded-r-full md:rounded-r-xl px-4 top-[50%] translate-y-[-37%]">
+        <div className='flex container mx-auto md:w-[70%] pt-5 px-6 justify-center items-center'>
+            <input className='rounded-l-full rounded-r-none  md:rounded-l-xl border py-[10px] md:py-[11px] px-5  w-full outline-none shadow-lg  ' placeholder='Search any term you like'/>
+            <div className="bg-primary md:py-[8px] py-[9px] rounded-r-full md:rounded-r-xl px-4">
               <IoSearch className="w-7 h-7 md:w-8 md:h-8 fill-white"/>
             </div>
-            <input className='rounded-full md:rounded-xl mt-5 mb-2 border py-[10px] md:py-[11px] px-5  w-full outline-none shadow-lg  ' placeholder='Search any term you like'/>
         </div>
         <IndexBrandsDiv/>
         <IndexPostsDiv/>
--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -3,19 +3,10 @@ import Image from "next/image";
 function Product() {
     return (
         <div className='flex bg-n navbar m-10 border container mx-auto w-full'>
-          <div className="w-[50%] py-10 px-10 flex">
-            <div className="flex flex-col gap-5">
-              <Image src='/product.webp' height={100} width={150}/>
-              <Image src='/product.webp' height={100} width={150}/>
-              <Image src='/product.webp' height={100} width={150}/>
-            </div>
-            <Image src='/product.webp' height={500} width={500}/>
+          <div className="md:w-[40%] bg-red-200">
+            <Image src='/3.webp' height={500} width={500} className="w-full"/>
           </div>
-          <div className="w-[50%] px-20 py-20">
-            <h1 className="text-[30px] font-serif">Nike font 12</h1>
-            <p className="text-[20px] font-serif">Nike</p>
-            <p className="pt-5 text-[30px] font-serif"><span className="line-through">100</span> <span className="text-primary">99</span></p>
-          </div>
+
         </div>
     );
 }
